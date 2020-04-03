3271b21401d7696d75418113632894758bdeaa39
Lucas Lima <luc.tech3337@gmail.com>
2020-04-02 23:25:06 -0300

    Update gerenciador_de_senhas.py

--- gerenciador_de_senhas.py
+++ gerenciador_de_senhas.py
@@ -1,6 +1,6 @@
 import random
 wi = '=' * 16
-fri = f'{wi} GERENCIADOR DE SENHAS {wi}'
+fri = f'{wi} GERENCIADOR DE CONTAS {wi}'
 print(f'{fri:^50}')
 print('')
 e1 = 2
@@ -17,7 +17,7 @@ Sua escolha: '''))
         while e1 == 1:
             num = random.randint(1, 100)
             social = str(input('\nDigite o nome de uma rede social ou site: ')).strip().title()
-            user = str(input('Digite o seu nome de usuario: ')).strip()
+            user = str(input('Digite o seu nome de usuário: ')).strip()
             password = str(input('Digite a sua senha: ')).strip()
             c = open('contas.txt', 'a')
             c.write('=-=' * 10)
@@ -39,27 +39,36 @@ Sua escolha: '''))
         for linhas in c:
             print(linhas)
         excluir = str(input('Digite o ID da conta que deseja excluir: ')).strip()
-        b = open('contas.txt').read().split()
-        if excluir == b[4]:
-            print('PROBLEMAS AQUI')
+        b = open('contas.txt')
+        q = b.readlines()
+        for num in range(len(q)):
+            if excluir in q[num]:
+                for ex in range(len(q)):
+                    q[num - 1] = q[num] = q[num + 1] = q[num + 2] = ''
+                    q[num + 3] = q[num + 4] = q[num + 5] = q[num + 6] = ''
+                b = open('contas.txt', 'w')
+                b.writelines(q)
+                b.close()
+        print('\nConta excluída com Sucesso!!\n')
     elif e == 3:
         c = open('contas.txt').read().splitlines()
         for ler in c:
             print(ler)
-        senha = str(input('\nDigite a senha que deseja mudar: ')).strip()
+        senha = str(input('\nDigite o ID da conta que deseja mudar a senha: ')).strip()
         novasenha = str(input('Digite uma nova senha: ')).strip()
         b = open('contas.txt')
         q = b.readlines()
         for num in range(len(q)):
             if senha in q[num]:
-                q[num] = f'Senha: {novasenha}\n'
+                q[num + 4] = f'Senha: {novasenha}\n'
                 b = open('contas.txt', 'w')
                 b.writelines(q)
                 b.close()
+        print('\nSenha trocada com Sucesso!!\n')
     elif e == 4:
         c = open('contas.txt').read().splitlines()
         for ler in c:
             print(ler)
     elif e == 5:
         e1 = 0
-        print('\nPrograma encerrado com sucesso!')
+        print('\nPrograma encerrado com sucesso!\n')
